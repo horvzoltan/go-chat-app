1d78a40aff7b859e5754dcc94080c73b8ce111d8
horvzoltan <zoltanhorvath0721@gmail.com>
2024-03-25 16:13:25 +0100

    update

--- web/script.js
+++ web/script.js
@@ -1,21 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var ws = new WebSocket("ws://localhost:8080/ws");
-    ws.onmessage = function (event) {
-        console.log('onmessage called');
-        var log = document.getElementById("log");
-        log.innerHTML += event.data + "<br>";
-    };
+  var ws = new WebSocket("ws://localhost:8080/ws");
+  var log = document.getElementById("log");
+  var messageInput = document.getElementById("messageInput");
+  var sendButton = document.getElementById("sendButton");
 
-    ws.onopen = function (event) {
-        ws.send(JSON.stringify({ message: "Hello Server!" }));
-        console.log('WebSocket connection opened');
-    };
+  ws.onopen = function (event) {
+    console.log("WebSocket connection opened");
+  };
 
-    ws.onclose = function (event) {
-        console.log('WebSocket connection closed');
-    };
+  ws.onclose = function (event) {
+    console.log("WebSocket connection closed");
+  };
 
-    ws.onerror = function (event) {
-        console.error('WebSocket error observed:', event);
-    };
+  ws.onerror = function (event) {
+    console.error("WebSocket error observed:", event);
+  };
+
+  ws.onmessage = function (event) {
+    console.log("onmessage called");
+    log.innerHTML += event.data + "<br>";
+  };
+
+  sendButton.onclick = function () {
+    var message = messageInput.value;
+    if (message) {
+      ws.send(JSON.stringify({ message: message }));
+      messageInput.value = "";
+    }
+  };
 });
